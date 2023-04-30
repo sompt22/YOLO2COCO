390f8b7aab9aaf69fd4f117b72e0b02b467508bf
sompt22 <femres87@gmail.com>
2023-04-30 16:18:42 +0300

    Yolo to MOTChallenge format conversion script updated

--- yolo2mot.py
+++ yolo2mot.py
@@ -1,59 +1,70 @@
-import os.path as osp
 import os
-import numpy as np
+import cv2
 
 def mkdirs(d):
-    if not osp.exists(d):
-        os.makedirs(d)
+    os.makedirs(d, exist_ok=True)
 
-label_root = '/data/yfzhang/MOT/JDE/MOT20/labels_with_ids/train'
-seq_root = '/data/yfzhang/MOT/JDE/MOT20/images/train'
-mkdirs(seq_root)
+# YOLO to MOTChallenge format conversion script
+def yolo_to_mot(input, output):
+    all_input_labels = f'{input}/labels_with_ids/train/'
+    print(all_input_labels)
+    all_input_images= f'{input}/images/train/'    
+    print(all_input_images)
 
-seqs = [s for s in os.listdir(label_root)]
+    seqs = os.listdir(all_input_labels)
+    for seq in seqs:
+        '''Inputs'''
+        input_image_path = f'{all_input_images}/{seq}/img1/'
+        input_label_path = f'{all_input_labels}/{seq}/img1/'
+        '''Outputs'''
+        output_image_path = f'{output}/train/{seq}/img1/'
+        output_gt_path = f'{output}/train/{seq}/gt/'
+        mkdirs(output_image_path)
+        mkdirs(output_gt_path)      
+        gt_txt = f'{output_gt_path}/gt.txt'
 
-for seq in seqs:
-    seq_label_root = osp.join(label_root, seq, 'img1')
-    gt_txt = osp.join(seq_label_root, 'gt.txt')
+        labels = os.listdir(input_label_path)
+        for label in labels:
+            frame = int(label.split('.')[0])
+            image = cv2.imread(f'{input_image_path}' + label.replace(".txt", ".jpg"))
+            height, width, _ = image.shape
+            cv2.imwrite(output_image_path + label.replace(".txt", ".jpg"), image)
 
-    with open(gt_txt, 'r') as f:
-        lines = f.readlines()
+            with open(f'{input_label_path}/{label}', 'r') as f:
+                annotations = f.readlines()
+            for annotation in annotations:
+                class_id, track_id, xcenter, ycenter, bbox_width, bbox_height = annotation.split()
 
-    mot_gt = []
+                bbox_abs_xcenter = float(xcenter)*width
+                bbox_abs_ycenter = float(ycenter)*height
+                bbox_abs_width = float(bbox_width)*width
+                bbox_abs_height = float(bbox_height)*height
+                              
+                bbox_xmin = float(bbox_abs_xcenter) - float(bbox_abs_width) / 2    # int(float(xcenter) - float(bbox_width) / 2)
+                bbox_ymin = float(bbox_abs_ycenter) - float(bbox_abs_height) / 2   #int(float(ycenter) - float(bbox_height) / 2)
+                bbox_xmax = float(bbox_abs_xcenter) + float(bbox_abs_width) / 2    #int(float(xcenter) + float(bbox_width) / 2)
+                bbox_ymax = float(bbox_abs_ycenter) + float(bbox_abs_height) / 2   #int(float(ycenter) + float(bbox_height) / 2)
+                
+                bbox_xmin = max(bbox_xmin, 0)
+                bbox_ymin = max(bbox_ymin, 0)
+                bbox_xmax = min(bbox_xmax, width - 1)
+                bbox_ymax = min(bbox_ymax, height - 1)
 
-    for line in lines:
-        line = line.strip().split(' ')
-        tid = int(line[1])
-        x = float(line[2])
-        y = float(line[3])
-        w = float(line[4])
-        h = float(line[5])
-        frame_id = int(line[0])
-        label = int(line[6])
-        mot_gt.append([frame_id, tid, x, y, w, h, label])
+                w = bbox_xmax - bbox_xmin
+                h = bbox_ymax - bbox_ymin
 
-    mot_gt = np.asarray(mot_gt)
+                bbox_xmin = int(bbox_xmin) 
+                bbox_ymin = int(bbox_ymin)
+                w = int(w)
+                h = int(h)
 
-    seq_info_path = osp.join(seq_root, seq, 'seqinfo.ini')
-    seq_info = open(seq_info_path).read()
-    seq_width = int(seq_info[seq_info.find('imWidth=') + 8:seq_info.find('\nimHeight')])
-    seq_height = int(seq_info[seq_info.find('imHeight=') + 9:seq_info.find('\nimExt')])
+                with open(gt_txt, "a") as f:
+                    label_str = '{},{},{},{},{},{},1,1,1.0\n'.format(frame, track_id, bbox_xmin, bbox_ymin, w, h)
+                    f.write(label_str)
+        print("{} finished!".format(seq))
 
-    mot_txt = osp.join(seq_root, seq, 'gt', 'gt.txt')
-    with open(mot_txt, 'w') as f:
-        for i in range(len(mot_gt)):
-            frame_id = mot_gt[i][0]
-            tid = mot_gt[i][1]
-            x = mot_gt[i][2]
-            y = mot_gt[i][3]
-            w = mot_gt[i][4]
-            h = mot_gt[i][5]
-            label = mot_gt[i][6]
-            if label == 0:
-                continue
-            x1 = int(x - w / 2.0)
-            y1 = int(y - h / 2.0)
-            x2 = int(x + w / 2.0)
-            y2 = int(y + h / 2.0)
-            f.write('{:d},{:d},{:.2f},{:.2f},{:.2f},{:.2f},1,-1,-1,-1\n'.format(
-                frame_id, tid, x1, y1, x2-x1, y2-y1))
+# Usage example
+input = '/home/fatih/phd/30042023/yolo/MOT17'
+output = '/home/fatih/phd/30042023/yolo/yolo2mot_MOT17'
+
+yolo_to_mot(input, output)
